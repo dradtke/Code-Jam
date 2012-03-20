20b90b8efaf4296a286567902ce931fb1d0dbc88
Damien Radtke <admin@damienradtke.org>
2012-03-20 15:51:46 -0500

    Removed unnecessary index-order check from haskell solution.

--- store-credit/store-credit.hs
+++ store-credit/store-credit.hs
@@ -12,23 +12,16 @@ main :: IO ()
 main = codeJam solve
 
 solve :: Input -> String
-solve input = formatAnswer res1 res2
+solve input = (show $ index res1) ++ " " ++ (show $ index res2)
     where (l1:l2:l3:_) = input
           credit = read l1 :: Int
           items = read l2 :: Int
           prices = map read $ take items $ (words l3) :: [Int]
           pricelist = zipWith Item [1..] prices
           options = [(item1,item2) | item1 <- pricelist, item2 <- pricelist,
-                                    (index item1) /= (index item2),
+                                    (index item1) < (index item2),
                                     isValid credit (item1,item2)]
           (res1,res2) = head options
 
 isValid :: Int -> (Item,Item) -> Bool
 isValid credit (item1,item2) = (price item1) + (price item2) == credit
-
-formatAnswer :: Item -> Item -> String
-formatAnswer item1 item2 = (show lowIndex) ++ " " ++ (show highIndex)
-    where i1 = index item1
-          i2 = index item2
-          lowIndex = min i1 i2
-          highIndex = max i1 i2
